5eb27c27a09d6fe0a5925e75db8a12725a27e098
Carlos Eduardo Carvalho <duducecg2006@gmail.com>
2026-07-05 19:52:46 -0300

    Migra envio de e-mails para Gmail e ajusta webhook

--- loja_poiesis/settings.py
+++ loja_poiesis/settings.py
@@ -131,9 +131,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'   # literalmente a string 'apikey'
-EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_API_KEY')
-DEFAULT_FROM_EMAIL = 'duducecg2006@gmail.com'  # seu e‑mail verificado no SendGrid
+EMAIL_HOST_USER = 'duducecg2006@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('hazk ncqa phba pmsj')
+DEFAULT_FROM_EMAIL = 'duducecg2006@gmail.com'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
